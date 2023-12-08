4bfd831e6842cc0bb40135fab2e7f7e1f39e9bb7
Blake McFarlane <blakescapital@gmail.com>
2023-12-08 16:29:49 -0500

    Adding 'update note' methods

--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 // import notes from '../assets/Data'
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams, useNavigate } from 'react-router-dom';
 import { ReactComponent as ArrowLeft } from '../assets/arrow-left.svg'
 
 const NotePage = () => {
@@ -18,16 +18,35 @@ const NotePage = () => {
         setNote(data)
     }
 
+    let updateNote = async() => {
+        await fetch(`http://localhost:8000/notes/${id}`,{
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify({...note, 'updated': new Date()})
+        })
+    }
+
+    let handleSubmit = () => {
+        updateNote()
+        navigate('/')
+    }
+
+    let navigate = useNavigate => {
+
+    }
+
     return (
         <div className='note'>
             <div className='note-header'>
                 <h3>
                     <Link to='/'>
-                        <ArrowLeft />
+                        <ArrowLeft onClick={handleSubmit}/>
                     </Link>
                 </h3>
             </div>
-            <textarea value={note?.body}>
+            <textarea onChange={(e)=> {setNote({...note, 'body':e.target.value})}} value={note?.body}>
 
             </textarea>
         </div>
